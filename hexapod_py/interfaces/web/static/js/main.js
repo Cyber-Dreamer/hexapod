d0482f28a3a193583a5ff590a912fe19f103692f
cyberdreamer <gata23laroche@gmail.com>
2025-11-07 11:25:23 -0500

    save

--- hexapod_py/interfaces/web/static/js/main.js
+++ hexapod_py/interfaces/web/static/js/main.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const stepHeightValue = document.getElementById('step-height-value');
 
     // --- State ---
-    let controlData = { vx: 0, vy: 0, omega: 0, pitch: 0, roll: 0, body_height: 200, standoff: 200, step_height: 40, power: false };
+    let controlData = { vx: 0, vy: 0, omega: 0, pitch: 0, roll: 0, body_height: 350, standoff: 200, step_height: 40, power: false };
     let sendInterval = null;
     let sensorData = { imu: {} }; // Shared state for sensor data
     let views = {}; // To hold our view elements
